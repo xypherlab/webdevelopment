8e192feb873d2d186cf95ebcc74c39fd64de1af4
xypherlab <122443943+xypherlab@users.noreply.github.com>
2023-03-19 14:24:47 +0800

    Update settings.py

--- Full Stack Projects/QR Digital Menu Builder/qrmenu_backend/qrmenu_backend/qrmenu_backend/settings.py
+++ Full Stack Projects/QR Digital Menu Builder/qrmenu_backend/qrmenu_backend/qrmenu_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-llgjw@(ulm@%(=!qw$c=%uo1kj9a76o+rdy9vi**dh$7qmh8bm'
+SECRET_KEY = 'Your Secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
